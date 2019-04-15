fd15bb09616085a5be96e065332e1548a08940b7
Carlos_Jorge <c.cuello@estudiantes.matcom.uh.cu>
2019-04-15 15:25:10 -0400

    cree un predicado accion que decide por el momento hacer un mov o  limp

--- Proyecto.pl
+++ Proyecto.pl
@@ -7,18 +7,17 @@ ambiente_inicial(N,M,PS,PO,PN):-(PS+PO+(2*PN))<100,
                                             cantidad(PN,C,PN_1),
                                                 cantidad(PS,C,PS_1),
                                                     cantidad(PO,C,PO_1),
-                                                        generar_conexo(T,corral,PN_1,S),
-                                                            generar(T,ninos,RT,PN_1),
-                                                                generar(S,sucia,RS,PS_1),
-                                                                    intersection(RT,RS,I),generar(I,obstaculo,_,PO_1).
+                                                        generar_obstaculos(T,C,RO,PO_1),
+                                                            generar_conexo(RO,corral,PN_1,S),
+                                                                generar(RO,ninos,_,PN_1),
+                                                                    generar(S,sucia,_,PS_1),
+                                                                        generar(RO,robot,_,1),dynamic(estado/1),robot(X,Y),ninos(X,Y),assert(estado(carga)).
 cantidad(P,T,C):-C is (P*T)//100.
-
 ambiente(N,M,T):-findall((X,Y),
                             (N_1 is N-1,M_1 is M-1,
-                                numlist(0,N_1,L1),numlist(0,M_1,L2),member(X,L1),member(Y,L2)
+                                numlist(0,N_1,L1),numlist(0,M_1,L2),member(X,L1),member(Y,L2),assert(posicion(X,Y))
                             ),
                                 T).
-
 %voy seleccion PS posiciones random de T y les aplica el predicado F a cada una de esas posiciones
 generar(T,_,T,0):-!.
 generar(T,F,Rest,PS):-random_select((X,Y),T,RestT),
@@ -30,6 +29,16 @@ ady((X,Y),(X_1,Y)):-X_1 is X+1.
 ady((X,Y),(X_1,Y)):-X_1 is X-1.
 ady((X,Y),(X,Y_1)):-Y_1 is Y+1.
 ady((X,Y),(X,Y_1)):-Y_1 is Y-1.
+ady2((X,Y),(X_1,Y)):-X_1 is X+2.                                        
+ady2((X,Y),(X_1,Y)):-X_1 is X-2.
+ady2((X,Y),(X,Y_1)):-Y_1 is Y+2.
+ady2((X,Y),(X,Y_1)):-Y_1 is Y-2.
+generar_obstaculos(T,C,Rest,PO):-NPO is C-PO,
+                                    generar_conexo(T,vacio,NPO,RestC),
+                                        findall((X,Y),vacio(X,Y),Rest),
+                                            retractall(vacio(X,Y)),
+                                                findall((X,Y),(member((X,Y),RestC),assert(obstaculo(X,Y))),_).
+
 generar_conexo(T,_,0,T):-!.
 generar_conexo(T,F,CC,R):-CC_1 is CC-1,
                     random_select((X,Y),T,T_1),
@@ -39,8 +48,8 @@ generar_conexo(T,F,CC,R):-CC_1 is CC-1,
 generar_conexo1(T,_,0,T):-!.
 generar_conexo1(T,F,CC,R):-CC_1 is CC-1,
                                 findall((X_1,Y_1),
-                                        %Verificamos todos los (X,Y) que cumplen F que son ady (X_1,Y_1) que no cumplen F
-                                            (XY=..[F,X,Y],XY,ady((X,Y),(X_1,Y_1)),member((X_1,Y_1),T),XY_1=..[F,X_1,Y_1],not(XY_1)),
+                                    %Verificamos todos los (X,Y) que cumplen F que son ady (X_1,Y_1) que no cumplen F
+                                        (XY=..[F,X,Y],XY,ady((X,Y),(X_1,Y_1)),member((X_1,Y_1),T),XY_1=..[F,X_1,Y_1],not(XY_1)),
                                 Adys),
                                 random_select((X_r,Y_r),Adys,_),
                                     select((X_r,Y_r),T, T_1),
@@ -48,7 +57,32 @@ generar_conexo1(T,F,CC,R):-CC_1 is CC-1,
                                             assert(C),
                                                 generar_conexo1(T_1,F,CC_1,R).
 
+swapPos((Xr,Yr),(X,Y),F):-Cr=..[F,Xr,Yr],retract(Cr),C=..[F,X,Y],assert(C).                                           
+movrobot(Xr,Yr,X,Y):-estado(carga),!,not(
+                                            (
+                                                not(ady((Xr,Yr),(X,Y))),
+                                                not(ady2((Xr,Yr),(X,Y)))
+                                            )
+                                        ),
+                                        not(obstaculo(X,Y)),
+                                        not(ninos(X,Y)),
+                                            posicion(X,Y),
+                                                swapPos((Xr,Yr),(X,Y),robot),swapPos((Xr,Yr),(X,Y),ninos).
+movrobot(Xr,Yr,X,Y):-ninos(X,Y),!,
+                        ady((Xr,Yr),(X,Y)),
+                            not(obstaculo(X,Y)),
+                                posicion(X,Y),
+                                    swapPos((Xr,Yr),(X,Y),robot),assert(estado(carga)).
+movrobot(Xr,Yr,X,Y):-ady((Xr,Yr),(X,Y)),
+                            not(obstaculo(X,Y)),
+                                posicion(X,Y),
+                                    swapPos((Xr,Yr),(X,Y),robot).
+
+limpiar(Xr,Yr):-sucia(Xr,Yr),retract(sucia(Xr,Yr)).
+% acciones
+accion(robot(Xr,Yr),limpiar,_,_):-limpiar(Xr,Yr).
+accion(robot(Xr,Yr),moverse,X,Y):-movrobot(Xr,Yr,X,Y).
 %Casillas que pueden estar juntas
-%sucio,niño
-%sucio,robot
-%corral,niño,robot
+% sucio,niño
+% sucio,robot
+% corral,niño,robot
--- Test.py
+++ Test.py
@@ -2,31 +2,67 @@ from pyswip import Prolog,call
 
 prolog=Prolog()
 prolog.consult("Proyecto.pl")
-N=5
-M=5
+N=7
+M=7
 PS=20
-PO=20
-PN=20
-
-tablero=""
-list(prolog.query("ambiente_inicial({0},{1},{2},{3},{4})".format(N,M,PS,PO,PN)))
-for i in range(N):
-    for j in range(M):
-        if(bool(list(prolog.query("corral({0},{1})".format(i,j))))):
-            if(bool(list(prolog.query("ninos({0},{1})".format(i,j))))):
-                tablero+="| N |"
+PO=10
+PN=30
+# tablero=""
+# list(prolog.query("ambiente({0},{1},T),generar_obstaculos(T,25,R,10)".format(N,M)))
+# for i in range(N):
+#     for j in range(M):
+#         if(bool(list(prolog.query("obstaculo({0},{1})".format(i,j))))):
+#                 tablero+="@@@@@"
+#         else:
+#             tablero+="     "
+#     tablero+="\n"
+# print(tablero)
+# for i in list(prolog.query("vacio(X,Y)")):
+#     print(i)
+def generar_ambiente(prolog):
+    list(prolog.query("ambiente_inicial({0},{1},{2},{3},{4})".format(N,M,PS,PO,PN)))
+def graficar_ambiente(prolog):
+    tablero=""
+    for i in range(N):
+        for j in range(M):
+            if(bool(list(prolog.query("corral({0},{1})".format(i,j))))):
+                if(bool(list(prolog.query("ninos({0},{1})".format(i,j)))) and bool(list(prolog.query("robot({0},{1})".format(i,j))))):
+                    tablero+="|B R|"
+                elif(bool(list(prolog.query("ninos({0},{1})".format(i,j))))):
+                    tablero+="| B |"
+                elif(bool(list(prolog.query("robot({0},{1})".format(i,j))))):
+                    tablero+="| R |"
+                else:
+                    tablero+="|   |"
+            elif(bool(list(prolog.query("ninos({0},{1})".format(i,j))))):
+                if(bool(list(prolog.query("sucia({0},{1})".format(i,j)))) and bool(list(prolog.query("robot({0},{1})".format(i,j))))):
+                    tablero+="#B#R#"
+                elif(bool(list(prolog.query("sucia({0},{1})".format(i,j))))):
+                    tablero+="##B##"
+                elif(bool(list(prolog.query("robot({0},{1})".format(i,j))))):
+                    tablero+=" B R "
+                else:
+                    tablero+="  B  "
+            elif(bool(list(prolog.query("sucia({0},{1})".format(i,j))))):
+                if(bool(list(prolog.query("robot({0},{1})".format(i,j))))):
+                    tablero+="##R##"
+                else:
+                    tablero+="#####"
+            elif(bool(list(prolog.query("obstaculo({0},{1})".format(i,j))))):
+                tablero+="@@@@@"
+            elif(bool(list(prolog.query("robot({0},{1})".format(i,j))))):
+                    tablero+="  R  "
             else:
-                tablero+="|   |"
-        elif(bool(list(prolog.query("ninos({0},{1})".format(i,j))))):
-            if(bool(list(prolog.query("sucia({0},{1})".format(i,j))))):
-                tablero+="##N##"
-            else:
-                tablero+="  N  "
-        elif(bool(list(prolog.query("sucia({0},{1})".format(i,j))))):
-            tablero+="#####"
-        elif(bool(list(prolog.query("obstaculo({0},{1})".format(i,j))))):
-            tablero+="@@@@@"
-        else:
-            tablero+="     "
-    tablero+="\n"
-print(tablero)
+                tablero+="     "
+        tablero+="\n"
+    print(tablero)
+generar_ambiente(prolog)
+graficar_ambiente(prolog)
+XY=list(prolog.query("robot(X,Y)"))[0]
+X=XY['X']
+Y=XY['Y']
+if(bool(list(prolog.query("movrobot({0},{1},{2},{3})".format(X,Y,X+1,Y))))):
+    print("Se pudo")
+else:
+    print("No se pudo")
+graficar_ambiente(prolog)
