a279b5404b2be2fbf16de2df537c79913432c4d9
Carlos_Jorge <c.cuello@estudiantes.matcom.uh.cu>
2019-04-26 21:47:01 -0400

    en el predicado program puse la opcion de que pueda mandarse el predicado

--- Proyecto.pl
+++ Proyecto.pl
@@ -70,35 +70,37 @@ accion(moverse,[((X1,Y1),D1),((X2,Y2),D2)],1):-
 p_casillas_sucias(P):-findall((X,Y), sucia(X,Y), Ls),findall((X,Y), posicion(X,Y), Lp),length(Ls,Cs),length(Lp,Cp),P is Cs/Cp.
 
 %  en el turno falta la talla de los ninos
-turno(T_p,T,D):-
+turno(Agente,T_p,T,D):-
     % random_agent(Accion,L,T_p,T),
-    agent1(Accion,L,T_p,T),
+    CallAgente=..[Agente,Accion,L,T_p,T],
+    % agent1(Accion,L,T_p,T),
+    CallAgente,
     accion(Accion,L,D),
     % ninos_turno(),
     graficar().
 % El ultimo valor de simulacion determina si se termina la simulacion o no
-simulacion(_,_,_,_,_,_,_,_,0):-!.
-simulacion(_,_,_,_,_,T,0,T,_):-
+simulacion(_,_,_,_,_,_,_,_,_,0):-!.
+simulacion(_,_,_,_,_,_,T,0,T,_):-
     !.
-simulacion(N,M,PS,PO,PN,T,C,T,_):-
+simulacion(Agente,N,M,PS,PO,PN,T,C,T,_):-
     !,
     C_1 is C-1,
     clear1(),
     ambiente_inicial(N,M,PS,PO,PN),
-    simulacion(N,M,PS,PO,PN,T,C_1,0,1).
-simulacion(N,M,PS,PO,PN,T,C,T_p,1):-
+    simulacion(Agente,N,M,PS,PO,PN,T,C_1,0,1).
+simulacion(Agente,N,M,PS,PO,PN,T,C,T_p,1):-
     p_casillas_sucias(P),
     P<0.6,!,
-    turno(T_p,T,D),
+    turno(Agente,T_p,T,D),
     % graficar(),
     T_p_1 is T_p+1,
-    simulacion(N,M,PS,PO,PN,T,C,T_p_1,D).
-simulacion(_,_,_,_,_,_,_,_,1):-
+    simulacion(Agente,N,M,PS,PO,PN,T,C,T_p_1,D).
+simulacion(_,_,_,_,_,_,_,_,_,1):-
     assert(estado(despedido)),
     write('Despedido'),
     nl.
 
-program(N,M,PS,PO,PN,T,C):-
+program(Agente,N,M,PS,PO,PN,T,C):-
     consult('ambiente.pl'),
     consult('conexidad.pl'),
     consult('agentes.pl'),
@@ -107,6 +109,6 @@ program(N,M,PS,PO,PN,T,C):-
     consult('graficar.pl'),
     consult('kid_move.pl'),
     ambiente_inicial(N,M,PS,PO,PN),
-    graficar().
-    % simulacion(N,M,PS,PO,PN,T,C,0,1).
+    graficar(),
+    simulacion(Agente,N,M,PS,PO,PN,T,C,0,1).
     % clear1().
